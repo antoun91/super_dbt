444db7b2cb33cfb03a2ec378b178aacb1b364440
Jacob A <homeboy7777@gmail.com>
2023-07-31 15:15:26 -0700

    crr fixes

--- models/kpis/crr.sql
+++ models/kpis/crr.sql
@@ -1,8 +1,8 @@
 select
   date_trunc('month', o.created_at::timestamp) as year_month, 
-  count(distinct o.customer_id) as active_users, 
-  count(distinct future_order.customer_id) as retained_users,
-  round(count(distinct future_order.customer_id)::decimal / count(distinct o.customer_id)::decimal, 2) as retention
+  count(distinct o.customer_id) as active_customers_this_month, 
+  count(distinct future_order.customer_id) as retained_customers_following_month,
+  round(count(distinct future_order.customer_id)::decimal / count(distinct o.customer_id)::decimal, 2) as retention_rate
 from super.order_data as o
 left join super.order_data as future_order on
   o.customer_id = future_order.customer_id
